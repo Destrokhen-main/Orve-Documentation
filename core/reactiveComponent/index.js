05f663ce2242e0e5562b361dce3b0872dbc8a42b
Vlad <armagiddon19@gmail.com>
2022-09-28 11:47:10 +0300

    add edit file from core

--- core/reactiveComponent/index.js
+++ core/reactiveComponent/index.js
@@ -1,14 +1,21 @@
-import { validatorTagNode } from "../core/linter";
-import { typeOf } from '../core/helper';
 import Type from "../type/proxy";
 
 import builder from "../core/builder";
 import { createNodeRebuild } from "../core/mount/rebiuld"
+import { typeOf } from "../core/helper";
 
 const s = (component) => {
+  let comp = component;
+  const type = typeOf(component);
+  if (type !== "function") {
+    if (type === "object") {
+      comp = () => component;
+    }
+  }
+
   const object = {
     parent: [],
-    value: component,
+    value: comp,
   }
 
   return new Proxy(object, {
@@ -21,11 +28,21 @@ const s = (component) => {
     },
     set(target, prop, value) {
       if (prop in target) {
+        let comp = value;
+        if (prop === "value") {
+          const type = typeOf(component);
+          if (type !== "function") {
+            if (type === "object") {
+              comp = () => value;
+            }
+          }
+        }
+
         target[prop] = value;
 
         if (prop === "value") {
           if (target.parent.length > 0) {
-            let newObj = builder(value);
+            let newObj = builder(comp);
             const object  = createNodeRebuild(null, newObj);
             target.parent = target.parent.map((el) => {
               el.insertAdjacentElement('afterend', object);
